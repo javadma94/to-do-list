be6cb8133dcac6bde36e3bd93c41c3b9d9888a90
john <javad.ma2013@gmail.com>
2024-04-14 22:06:49 +0330

    simple-structure

---
+++ todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    deadline = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
